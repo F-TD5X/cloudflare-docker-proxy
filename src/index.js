3e1107954a46ba97ca48329a4dce9e1df6b4df94
F_TD5X <mjikop1231@gmail.com>
2024-07-04 17:17:13 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.maye.workers.dev": dockerHub,
+  "quay.maye.workers.dev": "https://quay.io",
+  "gcr.maye.workers.dev": "https://gcr.io",
+  "k8s-gcr.maye.workers.dev": "https://k8s.gcr.io",
+  "k8s.maye.workers.dev": "https://registry.k8s.io",
+  "ghcr.maye.workers.dev": "https://ghcr.io",
+  "cloudsmith.maye.workers.dev": "https://docker.cloudsmith.io",
+  "ecr.maye.workers.dev": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.maye.workers.dev": dockerHub,
 };
 
 function routeByHosts(host) {
